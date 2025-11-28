a10b569be560c058fc06b7dd959f871244cff7ae
VedantKashid <vedantkashid15@gmail.com>
2025-11-28 15:14:48 +0530

    first-commit

---
+++ show.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Ethical Hacker", "Network Analyst", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
